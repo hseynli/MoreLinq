98d3c35f0e1f094805743c14e70bc06513ed49f9
Farid <hseynli@list.ru>
2021-06-14 22:49:03 +0400

    ..

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MoreLinq;
 using MoreLinq.Extensions;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,7 +11,16 @@ namespace MoreLinq
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            //------------------------------------------APPEND---------------------------------------------------------------------------------------
+            string[] fruits = { "apple", "banana", "orange", "cheryy" };
+
+            IEnumerable<string> result1 = fruits.RandomSubset(3);
+
+            
+            Console.WriteLine(result1.ToDelimitedString(","));
+
+            
+            //System.Linq.Enumerable.Append()
 
             Console.ReadKey();
         }
